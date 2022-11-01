77779ccb85468f5a6f352383c69277d523217549
brianrandall <brian19892@gmail.com>
2022-10-31 22:01:28 -0700

    axiosPokeAPI

---
+++ React/APIs/projects/AxiosPokemonAPI/client/src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import PokemonAxios from './components/pokemonAxios';
+
+function App() {
+  return (
+    <div className="App">
+      <PokemonAxios />
+    </div>
+  );
+}
+
+export default App;
---
+++ React/APIs/projects/AxiosPokemonAPI/client/src/components/pokemonAxios.jsx
@@ -0,0 +1,36 @@
+import React, {useState} from 'react'
+import axios from 'axios'
+
+const Pokemon = () => {
+
+    const [pokemon, setPokemon] = useState([])
+
+    const getPokemon = () => {
+        axios.get("https://pokeapi.co/api/v2/pokemon?limit=807")
+            .then(response => {
+                setPokemon(response.data)
+            })
+            .catch(err => {
+                console.log(err)
+            })
+
+    }
+
+  return (
+    <div>
+
+        <button onClick={getPokemon}>fetch pokemon axiosly</button>
+
+        {
+        pokemon.map((p, i) => {
+            return (
+                <div key={i}>
+                    {p.name}
+                </div>
+            )
+        })}
+    </div>
+  )
+}
+
+export default Pokemon
