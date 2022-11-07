219319c7f5262cb51865d8a63012956557049ce4
brianrandall <brian19892@gmail.com>
2022-11-06 17:29:54 -0800

    product updates

--- FSmern/projects/productManager/client/src/App.js
+++ FSmern/projects/productManager/client/src/App.js
@@ -2,6 +2,7 @@ import './App.css';
 import Main from './views/main'
 import Product from './views/productPage'
 import { Link, Routes, Route } from 'react-router-dom'
+import Update from './views/editProduct'
 
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
       <Routes>
         <Route path='/' element={ <Main /> } />
         <Route path='/:id' element={ <Product /> } />
+        <Route path='/edit/:id' element={ <Update /> } />
       </Routes>
     </div>
   );
