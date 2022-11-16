56d500a5e21040da00ef650c5da2d4e2ff514f33
brianrandall <brian19892@gmail.com>
2022-11-15 18:11:17 -0800

    ok ill stop tonight i guess

--- zProject/babylonDotCom/client/src/components/mainView.js
+++ zProject/babylonDotCom/client/src/components/mainView.js
@@ -1,23 +1,27 @@
 import React from 'react'
 import 'bootstrap/dist/css/bootstrap.css'
+import produce from './img/site_ads/produce.png'
+
 
 
 const ProductList = () => {
 
     return (
         <div>
+            
             <div className='adCarousel'>
+                <img src={require('./img/site_ads/produce.png')}/>
 
-                <div id="carouselExampleInterval" class="carousel slide" data-bs-ride="carousel">
+                {/* <div id="carouselExampleInterval" class="carousel slide" data-bs-ride="carousel">
                     <div class="carousel-inner">
                         <div class="carousel-item active" data-bs-interval="10000">
-                            <img src={require('./img/site ads/produce.png')} class="d-block w-100" alt="..."/>
+                            <img src={require('./img/site_ads/produce.png')} class="d-block w-100" alt='produce'/>
                         </div>
                         <div class="carousel-item" data-bs-interval="2000">
-                            <img src={require('./img/site ads/raw/big ones/holiday cheer.jpeg')} class="d-block w-100" alt="..."/>
+                            <img src={require('./img/site_ads/tools.png')} class="d-block w-100" alt='tools'/>
                         </div>
-                        <div class="carousel-item">
-                            <img src="..." class="d-block w-100" alt="..."/>
+                        <div class="carousel-item" data-bs-interval="2000">
+                            <img src={require('./img/site_ads/raw/squares/pexels-photo-868096.jpeg')} class="d-block w-100" alt="..."/>
                         </div>
                     </div>
                     <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleInterval" data-bs-slide="prev">
@@ -28,13 +32,30 @@ const ProductList = () => {
                         <span class="carousel-control-next-icon" aria-hidden="true"></span>
                         <span class="visually-hidden">Next</span>
                     </button>
-                </div>
+                </div> */}
 
 
 
             </div>
             <div className='categoryContainer'>
+                <div className='mainCategory'>
+                    <h3>chairs n things</h3>
+                    <img src={require('./img/site_ads/raw/squares/patio.jpeg')} width="200px"/>
+                </div>
+                <div className='mainCategory'>
+                    <h3>chairs n things</h3>
+                    <img src={require('./img/site_ads/raw/squares/patio.jpeg')} width="200px"/>
+                </div>
+                <div className='mainCategory'>
+                    <h3>chairs n things</h3>
+                    <img src={require('./img/site_ads/raw/squares/patio.jpeg')} width="200px"/>
+                </div>
+                <div className='mainCategory'>
+                    <h3>chairs n things</h3>
+                    <img src={require('./img/site_ads/raw/squares/patio.jpeg')} width="200px"/>
+                </div>
             </div>
+            
         </div>
     )
 }
--- zProject/babylonDotCom/client/src/views/home.js
+++ zProject/babylonDotCom/client/src/views/home.js
@@ -2,6 +2,7 @@ import React from 'react'
 import NavBar from '../components/navbar'
 import ProductList from '../components/mainView'
 
+
 const Home = () => {
   return (
     <div>
